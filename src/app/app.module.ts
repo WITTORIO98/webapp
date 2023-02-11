1a26f6442950bec6f4b5f0ccf238ecf9285750cf
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-02-11 12:02:04 +0100

    Create feedBack component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularHomeComponent } from './angular-home/angular-home.component';
 import { SetUpComponent } from './set-up/set-up.component';
+import { FeedBackComponent } from './feed-back/feed-back.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AngularHomeComponent,
-    SetUpComponent
+    SetUpComponent,
+    FeedBackComponent
   ],
   imports: [
     BrowserModule,
