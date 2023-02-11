5c2fb15caa81f70bc38fbb14f23af9da2ebdd9d1
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-02-11 10:55:21 +0100

    Move angular home to dedicated new page

---
+++ src/app/angular-home/angular-home.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-angular-home',
+  templateUrl: './angular-home.component.html',
+  styleUrls: ['./angular-home.component.scss']
+})
+export class AngularHomeComponent {
+  title = 'webapp';
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AngularHomeComponent} from "./angular-home/angular-home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'a', component: AngularHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
