983f2f13889674514d873e1178c8f53d62b7e994
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-02-23 15:38:47 +0100

    Add fullscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,13 +6,17 @@ import {Router} from "@angular/router";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp';
 
-  constructor(private router:Router) {
+  constructor(private router:Router, private elementRef: ElementRef) {
     this.autoRedirect('setUp');
   }
   autoRedirect(pageName:String):void{
     this.router.navigate([pageName]);
+  }
+  ngOnInit(): void {
+    const elem = document.documentElement; // Ottiene l'elemento radice
+    elem.requestFullscreen(); // Richiede la modalità fullscreen
   }
 }
