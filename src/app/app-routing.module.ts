ac4080d4bc970f63c7122eab3f0e5241c1971375
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-01 15:53:38 +0100

    Create secondary outlet for Routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SetUpComponent} from "./set-up/set-up.component";
+import {SimpleTextComponent} from "./tasks/simple-text/simple-text.component";
+import {SimpleMathComponent} from "./tasks/simple-math/simple-math.component";
+import {IlluminazioneEdgeComponent} from "./privacyIndicators/illuminazione-edge/illuminazione-edge.component";
+import {ClassicComponent} from "./privacyIndicators/classic/classic.component";
 
 const routes: Routes = [
-  {path: 'setUp', component: SetUpComponent},
+  {path: '', component: SetUpComponent, outlet: 'primary'},
+  {path: 'setUp', component: SetUpComponent, outlet: 'primary'},
+  {path: 'simple-text', component: SimpleTextComponent, outlet: 'primary'},
+  {path: 'simple-math', component: SimpleMathComponent, outlet: 'primary'},
+
+  {path: '', component: IlluminazioneEdgeComponent, outlet: 'secondary'},
+  {path: 'classic', component: ClassicComponent, outlet: 'secondary'},
+  {path: 'illuminazone-edge', component: IlluminazioneEdgeComponent, outlet: 'secondary'},
 ];
 
 @NgModule({
