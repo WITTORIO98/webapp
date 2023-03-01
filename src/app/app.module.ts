61b0419a0d96587aa84510164254dbb769ed185c
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-01 15:50:01 +0100

    Remove angulaHome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {AngularHomeComponent} from './angular-home/angular-home.component';
 import {SetUpComponent} from './set-up/set-up.component';
 import {FeedBackComponent} from './feed-back/feed-back.component';
 import {IlluminazioneEdgeComponent} from './privacyIndicators/illuminazione-edge/illuminazione-edge.component';
@@ -17,7 +16,6 @@ import {HttpClientModule} from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
-    AngularHomeComponent,
     SetUpComponent,
     FeedBackComponent,
     IlluminazioneEdgeComponent,
