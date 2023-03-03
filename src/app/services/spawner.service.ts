09cc981a4cb605eda3be1fec9f845de1844e915f
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-03 15:06:33 +0100

    Update randomIndicator()

--- src/app/services/spawner.service.ts
+++ src/app/services/spawner.service.ts
@@ -15,8 +15,8 @@ const indicators: string[] = [
   providedIn: 'root'
 })
 export class SpawnerService {
-  private readonly defaultDelay: number = 1;
-  private readonly defaultDuration: number = 1;
+  private readonly delay = {default: 2, rMin: 1, rMax: 3};
+  private readonly duration = {default: 2, rMin: 1, rMax: 3};
 
   private spawnedTasks: string[] = [];
   private spawnedIndicators: string[] = [];
@@ -61,18 +61,46 @@ export class SpawnerService {
   }
 
   /**
-   *
-   * @param delay     -1=default=random   dopo quanto tempo appare
-   * @param duration  -1=default=random   quanto tempo dura
+   * @param delay     '-1'=random
+   * @param duration  '-1'=random
    */
   public randomIndicator(delay?: number, duration?: number): void {
     if (delay == undefined) {
-      delay = this.defaultDelay;
+      delay = this.delay.default;
     }
     if (duration == undefined) {
-      duration = this.defaultDuration;
+      duration = this.duration.default;
+    }
+    if (delay == -1) {
+      delay = Math.floor(Math.random() *
+        (this.delay.rMax - this.delay.rMin + 1)) + this.delay.rMin;
+    }
+    if (duration == -1) {
+      duration = Math.floor(Math.random() *
+        (this.duration.rMax - this.duration.rMin + 1)) + this.duration.rMin;
     }
 
+    let notSpawnedIndicators: string[] = [];
+    indicators.forEach(indicator => {
+      let find: boolean = false;
+      this.spawnedIndicators.forEach(spawned => {
+        if (indicator == spawned) {
+          find = true;
+        }
+      });
+      if (!find) {
+        notSpawnedIndicators.push(indicator);
+      }
+    });
+
+    if (notSpawnedIndicators.length == 0) {
+      console.log("TUtti gli indicator sono stati schedulati");
+      this.scheduleIndicator(delay, duration, empty);
+    } else {
+      let rand = Math.floor(Math.random() * notSpawnedIndicators.length);
+      this.spawnedIndicators.push(notSpawnedIndicators[rand]);
+      this.scheduleIndicator(delay, duration, [notSpawnedIndicators[rand]]);
+    }
 
 
   }
