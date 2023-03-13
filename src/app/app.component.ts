8390440215c77a900ad65320049c53362f0912ce
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-13 12:22:01 +0100

    Move gazer code into eye-tracker service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import {Component, ElementRef, OnInit} from '@angular/core';
 import {SpawnerService} from "./services/spawner.service";
 import {Http2Service} from "./services/http2.service";
-
-// @ts-ignore
-const WebGazer: any = window.webgazer;
+import {EyeTrackerService} from "./services/eye-tracker.service";
 
 @Component({
   selector: 'app-root',
@@ -13,34 +11,22 @@ const WebGazer: any = window.webgazer;
 export class AppComponent implements OnInit {
   title = 'webapp';
 
-  constructor(private elementRef: ElementRef, private spawner: SpawnerService, private http: Http2Service) {
+  constructor(private elementRef: ElementRef,
+              private spawner: SpawnerService,
+              private eye: EyeTrackerService) {
 
   }
 
 
   ngOnInit(): void {
+    this.eye.start();
+
     const elem = document.documentElement; // Ottiene l'elemento radice
     elem.requestFullscreen(); //todo work?
 
     this.spawner.randomPage();
     this.spawner.randomIndicator();
-
-    this.gazer();
   }
 
-  private gazer() {
-    WebGazer.setGazeListener((data: any, elapsedTime: any) => {
-      if (data == null) {
-        return;
-      }
-      //console.log("X: " + data.x);//these x coordinates are relative to the viewport
-      //console.log("Y: " + data.y);//these y coordinates are relative to the viewport
-      //console.log(elapsedTime); //elapsed time is based on time since begin was called
-      // @ts-ignore
-      //document.getElementById("webgazerVideoContainer").style.setProperty('opacity', '0');  pointer-events: none;
-      //document.getElementById("webgazerGazeDot").style.setProperty('opacity', '0');         pointer-events: none;
-
-    }).begin();
-  }
 
 }
--- src/app/services/eye-tracker.service.ts
+++ src/app/services/eye-tracker.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+// @ts-ignore
+const WebGazer: any = window.webgazer;
+type coordinates = {
+  x: number,
+  y: number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EyeTrackerService {
+  private coord: coordinates = {x: 100, y: 100}
+  private initialized: boolean = false;
 
-  constructor() { }
+  constructor() {
+  }
+
+  public start() {
+    WebGazer.setGazeListener((data: any, elapsedTime: any) => {
+      if (data == null) {
+        return;
+      }
+      this.coord.x = data.x;    //these x coordinates are relative to the viewport
+      this.coord.y = data.y;    //these y coordinates are relative to the viewport
+      //console.log(elapsedTime); //elapsed time is based on time since begin was called
+
+      if (!this.initialized) {
+        // @ts-ignore
+        let webgazerVideoContainerStyle = document.getElementById("webgazerVideoContainer").style;
+        // @ts-ignore
+        let webgazerGazeDotStyle = document.getElementById("webgazerGazeDot").style;
+
+        webgazerVideoContainerStyle.setProperty('opacity', '0');
+        webgazerVideoContainerStyle.setProperty('pointer-events', 'none');
+
+        webgazerGazeDotStyle.setProperty('opacity', '0');
+        webgazerGazeDotStyle.setProperty('pointer-events', 'none');
+      }
+    }).begin();
+  }
+
+  public getCoord() {
+    return this.coord;
+  }
+
+  public getX() {
+    return this.coord.x;
+  }
+
+  public getY() {
+    return this.coord.y;
+  }
+
+
 }
