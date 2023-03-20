a5aa0955467713d3a7f375a0bd562909332ddbd2
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-20 12:31:38 +0100

    Update VibrationComponent

--- src/app/privacyIndicators/vibration/vibration.component.ts
+++ src/app/privacyIndicators/vibration/vibration.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-vibration',
   templateUrl: './vibration.component.html',
   styleUrls: ['./vibration.component.scss']
 })
-export class VibrationComponent {
+export class VibrationComponent implements OnInit {
+
+
+  ngOnInit(): void {//todo testing
+    if ('vibrate' in navigator) {
+      navigator.vibrate([200, 100, 200]); // specifica la durata della vibrazione in millisecondi
+      console.log('Vibrazione attivata');
+    } else {
+      console.log('API Web Vibration non supportata dal browser');
+    }
+
+  }
 
 }
