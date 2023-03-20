5fcd74c179ea1230b26a3b8d7acc1d07dff7b4f5
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-20 12:06:36 +0100

    Update classic-disco.component.ts

--- src/app/privacyIndicators/classic-disco/classic-disco.component.ts
+++ src/app/privacyIndicators/classic-disco/classic-disco.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-classic-disco',
   templateUrl: './classic-disco.component.html',
   styleUrls: ['./classic-disco.component.scss']
 })
-export class ClassicDiscoComponent {
+export class ClassicDiscoComponent implements OnInit, OnDestroy {
+  private variables = document.querySelector('.classicDiscoVariables');
+
+  ngOnInit(): void {
+    /*random*/
+    let innerDiameter: number = 3; //vw
+    let top: number = 6;  //vw
+    let right: number = 10; //vw
+    let color: string = 'red';
+    let border: number = 3; //vw
+  }
+
+  ngOnDestroy(): void {
+  }
 
 }
