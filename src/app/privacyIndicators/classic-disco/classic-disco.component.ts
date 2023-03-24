721dad2e11e175460182358c2dd707d4f29f299d
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-24 12:41:32 +0100

    Update classic-disco.component.ts

--- src/app/privacyIndicators/classic-disco/classic-disco.component.ts
+++ src/app/privacyIndicators/classic-disco/classic-disco.component.ts
@@ -13,12 +13,27 @@ export class ClassicDiscoComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    /*random*/
-    let innerDiameter: number = 3; //vw
-    let top: number = 6;  //vw
-    let right: number = 10; //vw
-    let color: string = 'red';
-    let border: number = 3; //vw
+    /*random?*/
+    let innerDiameter1: number = 3; //vw
+    let innerDiameter2: number = 6; //vw
+    let border1: number = 6; //vw
+    let border2: number = 3; //vw
+    let color1: string = 'green';
+    let color2: string = 'yellow';
+    /*random?*/
+    this.variables = document.querySelector('.classicDiscoVariables');
+    // @ts-ignore
+    this.variables.style.setProperty('--innerDiameter1', innerDiameter1 + "vw");
+    // @ts-ignore
+    this.variables.style.setProperty('--innerDiameter2', innerDiameter2 + "vw");
+    // @ts-ignore
+    this.variables.style.setProperty('--border1', border1 + "vw");
+    // @ts-ignore
+    this.variables.style.setProperty('--border2', border2 + "vw");
+    // @ts-ignore
+    this.variables.style.setProperty('--color1', color1);
+    // @ts-ignore
+    this.variables.style.setProperty('--color2', color2);
 
     this.http.spawnPrivacy(this.constructor.name);
   }
