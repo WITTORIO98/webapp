b5e3baa77831fb8e9d4183bd8dbb3de407069a76
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-03-24 11:24:10 +0100

    Add back-end call to classic-disco commponent

--- src/app/privacyIndicators/classic-disco/classic-disco.component.ts
+++ src/app/privacyIndicators/classic-disco/classic-disco.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Http2Service} from "../../services/http2.service";
 
 @Component({
   selector: 'app-classic-disco',
@@ -8,6 +9,9 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 export class ClassicDiscoComponent implements OnInit, OnDestroy {
   private variables = document.querySelector('.classicDiscoVariables');
 
+  constructor(private http: Http2Service) {
+  }
+
   ngOnInit(): void {
     /*random*/
     let innerDiameter: number = 3; //vw
@@ -15,9 +19,12 @@ export class ClassicDiscoComponent implements OnInit, OnDestroy {
     let right: number = 10; //vw
     let color: string = 'red';
     let border: number = 3; //vw
+
+    this.http.spawnPrivacy(this.constructor.name);
   }
 
   ngOnDestroy(): void {
+    this.http.despawnPrivacy(this.constructor.name);
   }
 
 }
