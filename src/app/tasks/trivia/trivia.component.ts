75c39798f96e5533f715ac9eb060469aa40704a1
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-04-29 12:13:00 +0200

    Create trivia

--- src/app/tasks/trivia/trivia.component.ts
+++ src/app/tasks/trivia/trivia.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {GET, Http2Service} from "../../services/http2.service";
+import {SpawnerService} from "../../services/spawner.service";
 
 @Component({
   selector: 'app-trivia',
   templateUrl: './trivia.component.html',
   styleUrls: ['./trivia.component.scss']
 })
-export class TriviaComponent {
+export class TriviaComponent implements OnInit, OnDestroy {
+  questions: any;// : { text: string, answers: { text: string, value: string }[] }[]
+  currentAnswers: any;
+  currentQuestion: string = "";
+  //todo parse url {url, space, question}
+  source: string = "";
+  url: boolean = false;
+  end: boolean = false;
+
+
+  constructor(private http: Http2Service, private spawner: SpawnerService) {
+    http.http.get(http.getUrl(GET.triviaQuestions), http.options).subscribe(data => {
+      this.questions = data;
+      this.nextQuestion();
+    });
+  }
+
+  nextQuestion(): void {
+    if (this.questions.length > 0) {
+      let quest = this.questions.shift();
+      this.currentQuestion = quest.text;
+      this.currentAnswers = quest.answers;
+      console.log(quest.answers);// todo
+    } else {
+      this.end = true;
+    }
+
+    console.log(this.currentQuestion);
+
+    if (this.currentQuestion.startsWith("http") || this.currentQuestion.startsWith("www") || this.currentQuestion.startsWith("assets")) {
+      this.source = this.currentQuestion.split(" ", 1)[0];
+      this.currentQuestion = this.currentQuestion.substring(this.source.length + 1);
+
+      this.url = true;
+    } else {
+      this.url = false;
+    }
+
+  }
+
+  nextStep(): void {
+    this.spawner.nextStep();
+  }
+
+  ngOnInit(): void {
+    this.http.spawnTask(this.constructor.name);
+  }
+
+  ngOnDestroy(): void {
+    this.http.despawnTask(this.constructor.name);
+  }
 
 }
