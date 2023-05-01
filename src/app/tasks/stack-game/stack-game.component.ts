ee6523f14c980da7ba67925ee34ebce4c40f7780
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-05-01 19:48:18 +0200

    Update stacGame.ts

--- src/app/tasks/stack-game/stack-game.component.ts
+++ src/app/tasks/stack-game/stack-game.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Http2Service} from "../../services/http2.service";
+import {SpawnerService} from "../../services/spawner.service";
 
 @Component({
   selector: 'app-stack-game',
   templateUrl: './stack-game.component.html',
   styleUrls: ['./stack-game.component.scss']
 })
-export class StackGameComponent {
+export class StackGameComponent implements OnInit, OnDestroy {
+
+  constructor(private http: Http2Service, private spawner: SpawnerService) {
+  }
+
+  nextStep(): void {
+    this.spawner.nextStep();
+  }
+
+  ngOnInit(): void {
+    this.http.spawnTask(this.constructor.name);
+  }
+
+  ngOnDestroy(): void {
+    this.http.despawnTask(this.constructor.name);
+  }
 
 }
