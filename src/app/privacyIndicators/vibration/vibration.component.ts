8f6368c35144c7be07f236f9644b6d6722085c9b
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-05-03 12:09:09 +0200

    Create public static vibrate()

--- src/app/privacyIndicators/vibration/vibration.component.ts
+++ src/app/privacyIndicators/vibration/vibration.component.ts
@@ -8,14 +8,21 @@ import {Component, OnInit} from '@angular/core';
 export class VibrationComponent implements OnInit {
 
 
-  ngOnInit(): void {//todo testing
+  ngOnInit(): void {
     if ('vibrate' in navigator) {
       navigator.vibrate([200, 100, 200]); // specifica la durata della vibrazione in millisecondi
       console.log('Vibrazione attivata');
     } else {
       console.log('API Web Vibration non supportata dal browser');
     }
+  }
 
+  public static vibrate() {
+    if ('vibrate' in navigator) {
+      navigator.vibrate([200, 100, 200]); // specifica la durata della vibrazione in millisecondi
+    } else {
+      console.log('API Web Vibration non supportata dal browser');
+    }
   }
 
 }
