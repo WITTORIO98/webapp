5ef49ecf5c766f57ccb330fd825803c2a12ff84e
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-05-03 12:11:24 +0200

    Update classic component to new eyeTracking

--- src/app/privacyIndicators/classic/classic.component.ts
+++ src/app/privacyIndicators/classic/classic.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Http2Service} from "../../services/http2.service";
+import {AlertZone, coordinates, EyeTrackerService} from "../../services/eye-tracker.service";
 
 @Component({
   selector: 'app-classic',
@@ -8,14 +9,25 @@ import {Http2Service} from "../../services/http2.service";
 })
 export class ClassicComponent implements OnInit, OnDestroy {
 
-  constructor(private http: Http2Service) {
+  constructor(private http: Http2Service, private eye: EyeTrackerService) {
   }
 
   ngOnInit(): void {
     this.http.spawnPrivacy(this.constructor.name);
+
+
+    const privacyIndicator = document.getElementById('privacyIndicator');
+    // @ts-ignore
+    const computedStyles = window.getComputedStyle(privacyIndicator);
+    const position: coordinates = {
+      x: parseInt(computedStyles.getPropertyValue('left').split(".")[0]),
+      y: parseInt(computedStyles.getPropertyValue('top').split(".")[0])
+    };
+    this.eye.setPrivacyIndicator(AlertZone.DEFAULT, position);
   }
 
   ngOnDestroy(): void {
+    this.eye.removePrivacyIndicator();
     this.http.despawnPrivacy(this.constructor.name);
   }
 
