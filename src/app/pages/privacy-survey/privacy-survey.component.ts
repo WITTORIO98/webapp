42b96aec2904cdaa83b11007118024fc6ca175d4
WITTORIO98 <60522229+WITTORIO98@users.noreply.github.com>
2023-05-10 14:02:56 +0200

    Create pryvacysurvey

--- src/app/pages/privacy-survey/privacy-survey.component.ts
+++ src/app/pages/privacy-survey/privacy-survey.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-privacy-survey',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./privacy-survey.component.scss']
 })
 export class PrivacySurveyComponent {
+  public one: boolean = true;
+  public two: boolean = false;
+  public three: boolean = false;
+  public answers: string[] = ["si", "no", "si, piu di una volta"];
 
+  public questions: { question: string, answer: string }[] = [];
+
+  updateAnswer(risposta: string, privacy: string) {
+    this.questions.push({question: privacy, answer: risposta});
+    
+    if (this.three) {
+      this.fine();
+    }
+    if (this.two) {
+      this.two = false;
+      this.three = true;
+    }
+    if (this.one) {
+      this.one = false;
+      this.two = true
+    }
+  }
+
+  private fine() {
+    return;
+  }
 }
